69cc9a80f26b773c49d678839513a2752d48b1fb
vladyslav1243 <galich.vinfort@gmail.com>
2020-10-05 17:57:43 +0300

    first commit

---
+++ app/js/video.js
@@ -0,0 +1,30 @@
+/*
+    Video
+ */
+
+(function() {
+    let videoMediaStyle = {
+        position: "absolute",
+        // left: "0",
+        // top: "0",
+        // bottom: "0",
+        // right: "0",
+        width: "100%",
+        height: "100%",
+        border: "none"
+    };
+
+    $(".video-media").css(videoMediaStyle);
+
+    $(".js-show-video").click(function() {
+        let id = $(this).data("video");
+        let parent = $(this).parent()[0];
+        let iframe =
+            '<iframe class="video-media" src="https://www.youtube.com/embed/' +
+            id +
+            '?autoplay=1&amp;loop=1&amp;&amp;playlist=Video_ID" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        $(parent).empty();
+        $(parent).append(iframe);
+        $(".video-media").css(videoMediaStyle);
+    });
+})();
